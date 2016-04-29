31ab2934ddf2f2b294c52316805b3d6dc8fa3679
Sam Rayner <sam@terracoding.com>
2016-04-29 12:25:58 +0100

    Scope theme image identifier validation to theme
    - In case multiple themes exist in the database

--- app/models/theme_image.rb
+++ app/models/theme_image.rb
@@ -2,7 +2,7 @@ class ThemeImage < ActiveRecord::Base
   belongs_to :theme, touch: true
 
   validates_presence_of :theme, :identifier
-  validates_uniqueness_of :identifier
+  validates_uniqueness_of :identifier, case_sensitive: false, scope: :theme_id
 
   mount_uploader :image, ImageUploader
   attr_accessor :base64
